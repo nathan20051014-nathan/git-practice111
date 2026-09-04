18c98e37e576efa3572326fa97629ff007733e92
nathan20051014-nathan <nathan20051014@gmail.com>
2026-09-04 18:00:22 +0800

    增加温度传感器读取功能

--- robot.py
+++ robot.py
@@ -7,4 +7,10 @@ def start_robot():
     print(f"Current speed: {speed}")
 
 
+def read_temperature():
+    temperature = -13.9
+    print(f"Temperature: {temperature} C")
+
+
 start_robot()
+read_temperature()
